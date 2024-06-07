104d14949fe35436f742a1f15b609641fbdd45e6
Juan Ignacio Ferrando Villoslada <97488974+JuanerArg@users.noreply.github.com>
2024-06-07 12:01:35 -0300

    Update Main.cpp

--- Nueva carpeta/src/Main.cpp
+++ Nueva carpeta/src/Main.cpp
@@ -1,9 +1,24 @@
-#include<iostream>
-#include<stdio.h>
+#define SDL_MAIN_HANDLED "SDL_MAIN"
+
+#include<cstdlib>
+#include<SDL2/SDL.h>
+#include<stdexcept>
+
 
 using namespace std;
 
 int main(){
-    cout << "Hola Mundo" << endl;
+    //Si falla SLD_init() avisar
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0) throw runtime_error{"ERROR => SDL_init()"};
+    
+    SDL_Window *window{SDL_CreateWindow("Test SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 720, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL)};
+    //Si falla SLD_CreateWindow avisar
+    if(not window) throw runtime_error("ERROR => SDL_CreateWindow()");
+    
+    //Renderizamos
+    SDL_Renderer *renderer{SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)};
+    
+    //si falla el renderer avisar
+    if(not renderer) throw runtime_error("ERROR => SDL_Renderer()");
     return 0;
-}   
+}
